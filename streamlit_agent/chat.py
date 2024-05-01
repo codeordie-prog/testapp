5a1c136080b9cecf0a8167a50a6d03ed311a5571
Kelvin Ndeti <kelvinndeti3142@gmail.com>
2024-05-01 20:35:57 +0300

    chat

--- streamlit_agent/chat.py
+++ streamlit_agent/chat.py
@@ -4,28 +4,30 @@ from langchain_core.prompts import PromptTemplate
 from langchain_openai import OpenAI
 from langchain_openai import OpenAI
 from langchain.chains import ConversationChain
-from langchain_core.prompts import HumanMessagePromptTemplate,ChatPromptTemplate
+from langchain_core.prompts import HumanMessagePromptTemplate,ChatPromptTemplate,MessagesPlaceholder
 from langchain_core.messages import AIMessage, HumanMessage,SystemMessage
 
 
 def chat(openai_key:str):
-    template = ChatPromptTemplate.from_messages(
-    [
-        SystemMessage(
-            content=(
-                "You are a smart assistant called 42. answer all queries accurately "
-                
-            )
-        ),
-        HumanMessagePromptTemplate.from_template("{text}"),
-    ]
-)
+    
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            SystemMessage(
+                content="""You are a very intelligent digital AI system that understands humans properly. Your name is 42,
+                you were named after the answer to the ultimate question in the hitch hikers guide to the galaxy.
+                Answer the user queries accurately.
+                """
+            ),  # The persistent system prompt
+            MessagesPlaceholder(
+                variable_name="chat_history"
+            ),  # Where the memory will be stored.
+            HumanMessagePromptTemplate.from_template(
+                "{human_input}"
+            ),  # Where the human input will injected
+        ]
+        )
 
-
-    prompt = PromptTemplate(
-       template=template
-    )
-    memory = ConversationBufferMemory(memory_key="chat_history")
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
     llm_chat = OpenAI(api_key=openai_key)
 
