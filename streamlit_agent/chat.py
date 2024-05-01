49c7adbc91ed5baeb77565e800f200a6973d8fd1
Kelvin Ndeti <kelvinndeti3142@gmail.com>
2024-05-01 11:13:22 +0300

    chat

---
+++ streamlit_agent/chat.py
@@ -0,0 +1,23 @@
+from langchain.chains import LLMChain
+from langchain.memory import ConversationBufferMemory
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import OpenAI
+
+template = """You are a funny chatbot called 42. You are named after the Hitch Hikers Guide to the Galaxy famous 42. having a conversation with a human.
+
+{chat_history}
+Human: {human_input}
+Chatbot:"""
+
+prompt = PromptTemplate(
+    input_variables=["chat_history", "human_input"], template=template
+)
+memory = ConversationBufferMemory(memory_key="chat_history")
+
+llm_chat = OpenAI()
+llm_chain = LLMChain(
+    llm=llm_chat,
+    prompt=prompt,
+    verbose=True,
+    memory=memory,
+)
