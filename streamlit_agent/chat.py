db7d61e242333a9290288c20db01f73d20aed68f
Kelvin Ndeti <kelvinndeti3142@gmail.com>
2024-05-01 12:19:19 +0300

    chat

--- streamlit_agent/chat.py
+++ streamlit_agent/chat.py
@@ -7,24 +7,24 @@ from langchain.chains import ConversationChain
 
 
 def chat(openai_key:str):
-    template = """You are a funny chatbot called 42. 
+    template = """You are a helpful smart assistant called 42. 
     You are named after the Hitch Hikers Guide to the Galaxy famous 42. having a conversation with a human.
 
     {chat_history}
     Human: {human_input}
-    Chatbot:"""
+    Assistant:"""
 
     prompt = PromptTemplate(
         input_variables=["chat_history", "human_input"], template=template
     )
-    memory = ConversationBufferMemory(memory_key="chat_history")
+    memory = ConversationBufferMemory(memory_key="chat_history",return_messages=False)
 
     llm_chat = OpenAI(api_key=openai_key)
 
     llm_chain = LLMChain(
     llm=llm_chat,
     prompt=prompt,
-    verbose=True,
+    verbose=False,
     memory=memory,
 )
 
