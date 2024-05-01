7187d2289060d99f15ae924bda2b68cc42689427
Kelvin Ndeti <kelvinndeti3142@gmail.com>
2024-05-01 12:44:33 +0300

    chat

--- streamlit_agent/chat.py
+++ streamlit_agent/chat.py
@@ -4,17 +4,24 @@ from langchain_core.prompts import PromptTemplate
 from langchain_openai import OpenAI
 from langchain_openai import OpenAI
 from langchain.chains import ConversationChain
+from langchain_core.prompts import HumanMessagePromptTemplate,ChatPromptTemplate
+from langchain_core.messages import AIMessage, HumanMessage,SystemMessage
 
 
 def chat(openai_key:str):
-    template = """You are a helpful smart assistant called 42.
-    You are named after the Hitch Hikers Guide to the Galaxy famous 42. 
-    answer all queries accurately and provide detailed explanation if needed.
+    template = ChatPromptTemplate.from_messages(
+    [
+        SystemMessage(
+            content=(
+                "You are a helpful assistant called 42. anser all queries accurately "
+                
+            )
+        ),
+        HumanMessagePromptTemplate.from_template("{text}"),
+    ]
+)
 
-    {chat_history}
-    
-    Human: {human_input}
-    Assistant:"""
+
     prompt = PromptTemplate(
         input_variables=["chat_history", "human_input"], template=template
     )
