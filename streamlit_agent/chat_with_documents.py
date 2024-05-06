92f4a94cf8271558d00f1b18d3e8962c1d7401bd
Kelvin Ndeti <kelvinndeti3142@gmail.com>
2024-05-06 17:07:08 +0300

    packed the code into readable functions

--- streamlit_agent/chat_with_documents.py
+++ streamlit_agent/chat_with_documents.py
@@ -48,6 +48,54 @@ Simply upload your document and start asking questions!
 
 
 try:
+    
+    class StreamHandler(BaseCallbackHandler):
+        def __init__(self, container: st.delta_generator.DeltaGenerator, initial_text: str = ""):
+            self.container = container
+            self.text = initial_text
+            self.run_id_ignore_token = None
+
+        def on_llm_start(self, serialized: dict, prompts: list, **kwargs):
+            # Workaround to prevent showing the rephrased question as output
+            if prompts[0].startswith("Human"):
+                self.run_id_ignore_token = kwargs.get("run_id")
+
+        def on_llm_new_token(self, token: str, **kwargs) -> None:
+            if self.run_id_ignore_token == kwargs.get("run_id", False):
+                return
+            self.text += token
+            self.container.markdown(self.text)
+
+
+    class PrintRetrievalHandler(BaseCallbackHandler):
+        def __init__(self, container):
+            self.status = container.status("**Context Retrieval**")
+
+        def on_retriever_start(self, serialized: dict, query: str, **kwargs):
+            self.status.write(f"**Question:** {query}")
+            self.status.update(label=f"**Context Retrieval:** {query}")
+        def on_retriever_end(self, documents, **kwargs):
+            for idx, doc in enumerate(documents):
+                source = os.path.basename(doc.metadata["source"])
+                self.status.write(f"**Document {idx} from {source}**")
+                self.status.markdown(doc.page_content)
+            self.status.update(state="complete")
+
+
+    #the interaction setup part
+    openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+
+    #chat setup
+    # Setup LLM and QA chain - msg variable for chat history from streamlitchatmessagehistory
+    #set up the memory with chat_memory as the msg variable -use conversational buffer memory
+    #set up the prompt
+    #initialize the llm with streaming true
+    #initialize the chain with all the set up fields i.e promp,memory,verbose false and llm
+    #use the chain to invoke chat query
+
     #function-1   
     @st.cache_resource(ttl="2h")
     def configure_retriever(uploaded_files):
@@ -108,54 +156,8 @@ try:
             file_name="my_text_file.txt",
             mime="text/plain"
         )
-
-    class StreamHandler(BaseCallbackHandler):
-        def __init__(self, container: st.delta_generator.DeltaGenerator, initial_text: str = ""):
-            self.container = container
-            self.text = initial_text
-            self.run_id_ignore_token = None
-
-        def on_llm_start(self, serialized: dict, prompts: list, **kwargs):
-            # Workaround to prevent showing the rephrased question as output
-            if prompts[0].startswith("Human"):
-                self.run_id_ignore_token = kwargs.get("run_id")
-
-        def on_llm_new_token(self, token: str, **kwargs) -> None:
-            if self.run_id_ignore_token == kwargs.get("run_id", False):
-                return
-            self.text += token
-            self.container.markdown(self.text)
-
-
-    class PrintRetrievalHandler(BaseCallbackHandler):
-        def __init__(self, container):
-            self.status = container.status("**Context Retrieval**")
-
-        def on_retriever_start(self, serialized: dict, query: str, **kwargs):
-            self.status.write(f"**Question:** {query}")
-            self.status.update(label=f"**Context Retrieval:** {query}")
-        def on_retriever_end(self, documents, **kwargs):
-            for idx, doc in enumerate(documents):
-                source = os.path.basename(doc.metadata["source"])
-                self.status.write(f"**Document {idx} from {source}**")
-                self.status.markdown(doc.page_content)
-            self.status.update(state="complete")
-
-
-    #the interaction setup part
-
-    openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
-    if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
-        st.stop()
-
-    #chat setup
-    # Setup LLM and QA chain - msg variable for chat history from streamlitchatmessagehistory
-    #set up the memory with chat_memory as the msg variable -use conversational buffer memory
-    #set up the prompt
-    #initialize the llm with streaming true
-    #initialize the chain with all the set up fields i.e promp,memory,verbose false and llm
-    #use the chain to invoke chat query
+    
+    #function-3 chat session
 
     st.cache_resource(ttl="2hr")
     def chat_with_42():
@@ -221,7 +223,8 @@ try:
                 #download button
             if st.button("Create and download txt"):
                 create_and_download(text_content=response['text'])
-                
+
+     #function-4 query documents           
     def query_documents():
         uploaded_files = st.sidebar.file_uploader(
             label="Upload files", type=["pdf","txt","csv"], accept_multiple_files=True
@@ -262,12 +265,13 @@ try:
                 stream_handler = StreamHandler(st.empty())
                 response = qa_chain.run(user_query, callbacks=[retrieval_handler, stream_handler])
 
-
+    #main function
     def main():
         chat_with_42()
         query_documents()
 
     
+    #call main
     main()
 
 
