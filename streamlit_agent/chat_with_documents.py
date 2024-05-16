572efe3c6efa4a4cb16b6f5d40ad6212a79f3f7c
Kelvin Ndeti <kelvinndeti3142@gmail.com>
2024-05-16 10:59:44 +0300

    logo

--- streamlit_agent/chat_with_documents.py
+++ streamlit_agent/chat_with_documents.py
@@ -23,45 +23,12 @@ from langchain_core.runnables.history import RunnableWithMessageHistory #for cha
 from langchain.prompts import PromptTemplate
 
 st.set_page_config(page_title="Ask Fortytwo", page_icon="👽", layout="centered")
-st.title("👽 Ask Fortytwo 🛸🌌")
-st.markdown("*Unlocking the mysteries of the universe, one question at a time*")
 
-# Explanation of the App
-st.header('About the App')
-st.write("""
-42, named after the answer to the ultimate question of life in the HitchHiker's Guide to the Galaxy, is an advanced question-answering platform that allows users to upload documents in the formats (pdf,txt and csv) and receive answers to
-their queries based on the content of these documents. 
-Utilizing RAG approach powered by OpenAI's GPT models, the app provides insightful and contextually relevant answers.
+# Create three columns
+col1, col2, col3 = st.columns([1,2,1])
 
-### How It Works
-- add your secret openAI API key on the top left slider.
-- for a basic chat without document query, use the chat_with_42 query entry
-         
-- for document query, upload a Document: You can upload any document in `.pdf`,`.txt`, or `.csv` format.
-- you can also upload multiple documents and query them all together.
-- Ask a Question: After uploading the document, type in your question related to the document's content.
-- Get Answers: AI analyzes the document and provides answers based on the information contained in it.
-         
-- Note : clear message history button resets the context of the conversation else, 42 might answer 'I don't know".
-
-""")
-
-#instructions
-# Instructions for getting an OpenAI API key
-st.subheader("Get an OpenAI API key")
-st.write("You can get your own OpenAI API key by following the instructions:")
-st.write("""
-1. Create an openAI account
-2. Go to [OpenAI API Keys](https://platform.openai.com/account/api-keys).
-3. Click on the `+ Create new secret key` button.
-4. Next, enter an identifier name (optional) and click on the `Create secret key` button.
-""")
-
-st.write("""
-### GET STARTED
-
-simply upload your documents and query 42, or just engage in a chat below:
-""")
+with col2:
+    st.image("logo/logo.png", width=450)
 try:
     
     class StreamHandler(BaseCallbackHandler):
