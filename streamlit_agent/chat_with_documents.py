ca656c213cc7da8bc8b6ed68a7d18971701ce66d
Kelvin Ndeti <kelvinndeti3142@gmail.com>
2024-05-22 10:56:14 +0300

    callback

--- streamlit_agent/chat_with_documents.py
+++ streamlit_agent/chat_with_documents.py
@@ -316,21 +316,22 @@ try:
                         prompt=system_prompt
                     )
 
-                    stream_handler = StreamHandler(st.empty())
+                    
 
                     # Append user message to session state
                     st.session_state["messages"].append({"role": "user", "content": user_input})
                     st.chat_message("user").write(user_input)
 
+                    stream_handler = StreamHandler(st.empty())
             
                                         # Get response from LLM chain
                     response = llm_chain.run({"question": user_input},callbacks = [stream_handler])
-                   #assistant_msg = response  # Adjusted to fetch text from the response
+                    assistant_msg = response  # Adjusted to fetch text from the response
 
                     
 
                     # Append assistant message to session state and display it
-                    #st.session_state["messages"].append({"role": "assistant", "content": assistant_msg})
+                    st.session_state["messages"].append({"role": "assistant", "content": assistant_msg})
                     #st.chat_message("assistant").write(assistant_msg)
 
                     # Download chat button
